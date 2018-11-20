6d4ccb8e2f72adad4a3e317bb65071d1d4776f86
Arthur-Milchior <arthur@milchior.fr>
2018-11-20 01:55:12 +0100

    init

---
+++ README.md
@@ -0,0 +1,22 @@
+
+# Description
+Allows to view HTML of each field of the selected card. Go to the
+browser, select some notes, go to edit->"Note's HTML". For each
+selected note, you'll have a text showing the fields and their
+html. This text is also printed of the standard output (assuming you
+launched anki from a terminal).
+# Rational
+This allows you to understand why a field which appear to be empty is
+not, to view the name of an image, to view that some tags are opened
+and not closed (or reciprocally)... i.e. I use it to debug my cards
+when I've strange problem
+
+## Links, licence and credits
+
+Key|Value
+---|---
+Copyright|Arthur Milchior <arthur@milchior.fr>
+Based on|Anki code by Damien Elmes <anki@ichi2.net>
+License|GNU AGPL, version 3 or later; http|//www.gnu.org/licenses/agpl.html
+Source in| https://github.com/Arthur-Milchior/anki-show-html
+Addon number| 1778098423  https://ankiweb.net/shared/info/1778098423
---
+++ showHtml.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""Copyright: Arthur Milchior arthur@milchior.fr
+
+"""
+
+from aqt.utils import tooltip, showWarning, askUser
+from PyQt5.QtWidgets import *
+from anki.hooks import addHook
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from aqt import mw
+import cgi
+import anki.notes
+
+
+def HTML(nids):
+    """Show each field's content, and the note id. Click on cancel to stop seeing ids"""
+    for nid in nids:
+        note = mw.col.getNote(nid)
+        text= "\n"
+        for (name,value) in note.items():
+            text = text+ "\n"+ name + u":\n« "+ cgi.escape(value)+u"\n<br/>--------------<br/>\n"
+        text = text+ "\nnid: "+ str(note.id)
+        print(text)
+        if not askUser(text):
+            return
+
+def setupMenu(browser):
+    a = QAction("Note's HTML", browser)
+    a.setShortcut(QKeySequence("Ctrl+H")) # Shortcut for convenience. Added by Didi
+    a.triggered.connect(lambda e: onHTML(browser))
+    browser.form.menuEdit.addSeparator()
+    browser.form.menuEdit.addAction(a)
+
+def onHTML(browser):
+    HTML(browser.selectedNotes())
+
+addHook("browser.setupMenus", setupMenu)
+
